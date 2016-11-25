2cd2e1182d772ec3b000166ec882235870334518
TSRlightda <lightda104530@gmail.com>
2016-11-25 23:50:35 +0800

    Update TFast.java

--- src/tfast/java/tw/tsr/tfast/TFast.java
+++ src/tfast/java/tw/tsr/tfast/TFast.java
@@ -1,1 +1,45 @@
 package tw.tsr.tfast
+
+import tw.tsr.command.CommandReader;
+import tw.tsr.utils.MainLogger;
+import tw.tsr.utils.ServerKiller;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ **/
+
+/**
+ *
+ *
+ * @author TSRlightda(code) @ TFast Project
+ * @author DockCreaTer Team(javadoc) @ Tfast Project
+ * @since TFast 0.1 | TFast API 0.0.1
+ *
+ *
+ **/
+
+public class TFast {
+  
+  public final static String VERSION = "0.1 dev";
+    public final static String API_VERSION = "0.0.1";
+      public final static String CODENAME = "TSR(童話之星) TFast(星速)";
+        public final static String MINECRAFT_VERSION = "v.0.16.0 alpha";
+          public final static String MINECRAFT_VERSION_NETWORK = "0.16.0";
+  
+  public final static String PATH = System.getProperty("user.dir") + "/";
+    public final static String DATA_PATH = System.getProperty("user.dir") + "/";
+      public final static String PLUGIN_PATH = DATA_PATH + "plugins";
+        public static final long START_TIME = System.currentTimeMillis();
+          public static boolean ANSI = true;
+            public static boolean shortTitle = false;
+              public static int DEBUG = 1;
+  
