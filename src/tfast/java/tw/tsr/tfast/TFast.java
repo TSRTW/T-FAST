0a5faded981774d53745008828242ab1704fd39e
TSRlightda <lightda104530@gmail.com>
2016-11-26 09:10:14 +0800

    Mmm... Update API version and NETWORK version

--- src/tfast/java/tw/tsr/tfast/TFast.java
+++ src/tfast/java/tw/tsr/tfast/TFast.java
@@ -30,10 +30,12 @@ import tw.tsr.killer.ServerKiller;
 public class TFast {
   
   public final static String VERSION = "0.1 dev";
-    public final static String API_VERSION = "0.0.1";
+    public final static String API_VERSION = "1.0.0";
       public final static String CODENAME = "TSR(童話之星) TFast(星速)";
-        public final static String MINECRAFT_VERSION = "v.0.16.0 alpha";
-          public final static String MINECRAFT_VERSION_NETWORK = "0.16.0";
+        public final static String MINECRAFT_PC_VERSION = "1.9";
+          public final static String MINECRAFT_PE_VERSION = "0.16.0 alpha";
+            public final static String MINECRAFT_PE_VERSION_NETWORK = "0.16.0";
+              public final static String MINECRAFT_PC_VERSION_NETWORK = "1.9";
   
   public final static String PATH = System.getProperty("user.dir") + "/";
     public final static String DATA_PATH = System.getProperty("user.dir") + "/";
